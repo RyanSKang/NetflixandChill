d39697c91234e4e218818ad4c3c734bc13f31f97
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-05-30 21:34:25 -0600

    redoing HTML and putting it in handlebar

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let searchBtn=$('.searchBtn');
 let searchBar=$('.searchBar');
 let movieVal=document.getElementById('movieInput');
 let resultsGrid=$('.searchGrid');
-let searchList=$('.searchList');
 let searchHistArr=[];
 
 // Fetching the API
@@ -18,24 +17,10 @@ getMovieResults('avengers');
 // Searching movies from API based on search Bar text input
 function searchMovies(){
     let searchTerm=movieVal.value.trim();
-    // console.log(searchTerm) 
-    if(searchTerm.length > 0){
-        searchList.classlist.remove('.hideSearchList');
-    } else {
-        searchList.classList.add('.hideSearchlist');
-    }
+    console.log(searchTerm) 
 }
 
-// Display movie results from the API based on search Bar text input
-// function displayResult(movies){
-
-// }
-
-searchBtn.on('click', function (event){
-    event.preventDefault();
-    searchHistArr.push(textVal.value);
-    console.log(textVal.value)
-    localStorage.setItem('movies', JSON.stringify(searchHistArr));
-});
-
-
+// handlebar needs to make component that will fill out the necessary requirements of api data into 
+// the page
+// and in order to make this work i will need to route the handle bar so when search button is clicked 
+// 
