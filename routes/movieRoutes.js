ad619ee11732fe0bb2d7de7fcb085a170e0aad8e
yaneyalvarado <alvyaney@gmail.com>
2023-05-30 19:56:28 -0600

    linked movieroutes.js to synapsis.html, required express in movieroutes.js, installed express, sequelize, authentication(auth0), dotenv, and mysql 2

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,20 +1,23 @@
+const router = require('express').Router();
+
 router.get('/synapsis/:movie', async (req, res) => {
     try {
         async function getMovieResults(searchTerm){
             const URL=`https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=6d03d3e5`;
             const res= await fetch(`${URL}`);
             const data= await res.json();
-            console.log(data)
             res.render('synapsis', {
                 // the data will be sent into the synapsis.handlebars
         ...data,
         logged_in: req.session.logged_in
       });
     };
-        
+          console.log(data)
         getMovieResults(req.params.movie);
       
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+  module.exports = router;
