977fe2e806fc1c838026e9bd30035be208ce5d17
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-03 15:19:59 -0600

    working on mainpage js.

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -1,19 +1,22 @@
 // Defining variables for search bar
 let searchBtn = $('.searchBtn');
 let searchBar = $('.searchBar');
-let movieVal = $('#movieInput');
+let movieVal = document.getElementById('movieInput').value;
 let resultsGrid = $('.searchGrid');
 let searchHistArr = [];
 
 // 1. Define search value from user interaction 
 // 2. save search value and save it into local storage within the array
+function submitSearch(){
+    console.log(movieVal);
+};
+
 searchBtn.on('click', function (event) {
     event.preventDefault();
-    console.log('clicked');
-    searchHistArr.push(movieVal.value);
-    localStorage.setItem('Title', json.parse(movieVal.value));
-    // console.log(movieVal.value);
-})
+    searchHistArr.push(movieVal);
+    localStorage.setItem('Title', JSON.stringify(searchHistArr));
+    submitSearch();
+});
 // 3. use search value into getMovieResults() in our homeroutes.js
 
 
--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
         })
     };
     try{
-    getMovieResults('avengers');
+    getMovieResults('scream');
 
 } catch (err) {
     res.status(500).json(err);
