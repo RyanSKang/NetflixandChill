baed8e212a564da32b07cea60e820f7b45a93540
yaneyalvarado <alvyaney@gmail.com>
2023-06-03 06:54:20 -0600

    Updated line 39 in homepage.handlebars to link the synapsis. Changed lines 7 and 9 in the synapsis.handlebars to sync the movie title and the full plot (synapsis). Moved async function from movieroutes.js to homeroutes.js to successfully fetch the api and be able to get the plot directly from the homepage.

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -32,4 +32,38 @@ router.get('/', async (req, res) => {
 }
 });
 
+router.get('/synapsis/:movie', async (req, res) => {
+    try {
+        async function getMovieResults(title){
+            // const URL=`https://www.omdbapi.com/?s=${title}&page=1&apikey=6d03d3e5`;
+            // added new link to get the full plot of the movie
+            const URL="http://www.omdbapi.com/?t=The+Avengers&y=2012&plot=full&apikey=6d03d3e5";
+            const response= await fetch(`${URL}`);
+            const data= await response.json();
+            console.log(data)
+            res.render('synapsis', {
+                // the data will be sent into the synapsis.handlebars
+        ...data,
+        // logged_in: req.session.logged_in
+      });
+    };
+          // console.log(data)
+        getMovieResults(req.params.movie);
+   
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // Display movie results from the API based on search Bar text input
+
+//   searchBtn.on('click', function (event){
+//     event.preventDefault();
+//     searchHistArr.push(textVal.value);
+//     console.log(textVal.value)
+//     localStorage.setItem('movies', JSON.stringify(searchHistArr));
+// });
 module.exports = router;
+
+
