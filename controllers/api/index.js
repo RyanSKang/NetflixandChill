7f2f36b03d568ce5337618fd19023c7a73dd7838
Wes Simcox <wes.simcox11@gmail.com>
2023-06-05 20:27:40 -0600

    fixing routing issues

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const movieRoutes = require('./movieRoutes');
 const homeRoutes= require('../homeroutes');
+const login= require('../api/login');
 
 router.use('/', homeRoutes);
 router.use('/movieapi', movieRoutes);
+router.use('/login', login);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./user');
+const User = require('./user.js');
 const Favorite = require('./Favorite');
 
 User.hasMany(Favorite, {
