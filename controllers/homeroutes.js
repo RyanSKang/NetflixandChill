3b3c7487d1dbe933752cd1f741826489feca7c85
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-05 18:37:02 -0600

    created getmovieresults js to handle function

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,6 +1,7 @@
 // Adding Dependencies
 const router = require('express').Router();
-const fetch= require('node-fetch');
+const fetch = require('node-fetch');
+const {getMovieResults} = require('../utils/getMovieResults');
 
 // Registering public folder to express 
 var express = require('express');
@@ -8,67 +9,64 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 // Fetching the API using try and catch() for GET Route
-router.get('/', async (req, res) => {
-    let limitedResults=[];
-        async function getMovieResults(searchTerm){
-        const URL=`https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=6d03d3e5`;
-        const response= await fetch(URL)
-        .then(response =>{return response.json()})
-        .then(data => {  
-            for(i=0; i<10; i++){
-            limitedResults.push(data.Search[i])};
-            console.log(limitedResults);
+router.get('/', (req, res) => {
+    try {
+        getMovieResults('avengers').then(data =>{
+            console.log(data);
             res.render('homepage', {
-                limitedResults,
+                data,
                 // logged_in: req.session.logged_in
-            });
+            })
         })
-    };
-    try{
-    getMovieResults('avengers');
-
-} catch (err) {
-    res.status(500).json(err);
+    }
+    catch (err) {
+        res.status(500).json(err);
 }
 });
 
 // Middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-router.post('/movie', async (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-    // getMovieResults(req.body);
+router.post('/', (req, res) => {
+    try {
+        getMovieResults(req.body.Title).then(data =>{
+            console.log(data);
+            res.render('homepage', {
+                data,
+                // logged_in: req.session.logged_in
+            })
+        })
+    }
+    catch (err) {
+        res.status(500).json(err);
+}
 });
-
-
-
 
 router.get('/synapsis/', async (req, res) => {
     try {
-        async function getSynapsis(title, year){
+        async function getSynapsis(title, year) {
             // const URL=`https://www.omdbapi.com/?s=${title}&page=1&apikey=6d03d3e5`;
             // added new link to get the full plot of the movie
-            const URL=`http://www.omdbapi.com/?t=${title}&y=${year}&plot=full&apikey=6d03d3e5`;
-            const response= await fetch(`${URL}`);
-            const data= await response.json();
+            const URL = `http://www.omdbapi.com/?t=${title}&y=${year}&plot=full&apikey=6d03d3e5`;
+            const response = await fetch(`${URL}`);
+            const data = await response.json();
             console.log(data)
             res.render('synapsis', {
                 // the data will be sent into the synapsis.handlebars
-        ...data,
-        // logged_in: req.session.logged_in
-      });
-    };
-          // console.log(data)
+                ...data,
+                // logged_in: req.session.logged_in
+            });
+        };
+        // console.log(data)
         getSynapsis(req.params.movie);
-   
+
     }
     catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
+});
 
-  // Display movie results from the API based on search Bar text input
+// Display movie results from the API based on search Bar text input
 
 //   searchBtn.on('click', function (event){
 //     event.preventDefault();
--- models/user.js
+++ models/user.js
@@ -1,58 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+const {model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class User extends Model {}
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
 );
 
 module.exports = User;
